c0448ac3b011e1ffba3bdb052944afc6bf3a359e
mosc865c <moritz.schramm@mailbox.tu-dresden.de>
2024-11-29 17:36:47 +0100

    Adapt formatting and environemt settings to  make comparisons to other branches more easy

--- lightning-background-processor/src/lib.rs
+++ lightning-background-processor/src/lib.rs
@@ -229,7 +229,8 @@ impl<
 		G,
 		&'a (dyn UtxoLookup + Send + Sync),
 		L,
-	> where
+	>
+where
 	L::Target: Logger,
 {
 	/// Initializes a new [`GossipSync::Rapid`] variant.
@@ -246,7 +247,8 @@ impl<'a, L: Deref>
 		&'a NetworkGraph<L>,
 		&'a (dyn UtxoLookup + Send + Sync),
 		L,
-	> where
+	>
+where
 	L::Target: Logger,
 {
 	/// Initializes a new [`GossipSync::None`] variant.
